e557f522d88bccfabb0e3115bd5eb4ad109fed26
Kaavya <kaavyacp@gmail.com>
2026-06-24 11:34:06 +0400

    removed website related things and replaced with kanban view

--- odoo_advc/tourism/__manifest__.py
+++ odoo_advc/tourism/__manifest__.py
@@ -1,15 +1,15 @@
+# -*- coding: utf-8 -*-
 {
-    'name':'Tourism Management',
-    'depends':['base', 'sale', 'fleet', 'website'],
-    'application':True,
-    'data' : [
+    'name': 'Tourism Management',
+    'depends': ['base', 'sale', 'fleet'], # Removed website dependency entirely!
+    'application': True,
+    'data': [
         "views/tourism_package_views.xml",
         "views/tourism_booking_views.xml",
         "views/tourism_package_type_views.xml",
         "views/tourism_package_tag_views.xml",
         "views/tourism_menus.xml",
         "views/res_users_views.xml",
-        "security/ir.model.access.csv",
-        "views/tourism_package_templates.xml"
+        "security/ir.model.access.csv"
     ]
 }
