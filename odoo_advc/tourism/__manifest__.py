78044e6b46516d4239d656bb509b644b22b4acb7
Kaavya <kaavyacp@gmail.com>
2026-06-24 17:51:03 +0400

    Now you can get an idea of the tourism package but there are bugs and errosr which need to be fixed

--- odoo_advc/tourism/__manifest__.py
+++ odoo_advc/tourism/__manifest__.py
@@ -4,12 +4,16 @@
     'depends': ['base', 'sale', 'fleet'], 
     'application': True,
     'data': [
+        "security/ir.model.access.csv",
+        
+        # Menus must be loaded BEFORE views that use them
+        "views/tourism_menus.xml",
+        
+        # Then load the views
         "views/tourism_package_views.xml",
         "views/tourism_booking_views.xml",
         "views/tourism_package_type_views.xml",
         "views/tourism_package_tag_views.xml",
-        "views/tourism_menus.xml",
         "views/res_users_views.xml",
-        "security/ir.model.access.csv"
     ]
 }
